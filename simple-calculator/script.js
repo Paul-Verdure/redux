307a52f65c9a97460e5af0029c8365f98cd230fe
Paul-Verdure <paul.verdure@gmail.com>
2021-09-28 18:52:56 +0200

    create index.html

--- simple-calculator/script.js
+++ simple-calculator/script.js
@@ -1,7 +1,17 @@
     
-let firstValue = window.prompt('Tapez la valeur.');
-let operator = window.prompt("Tapez l'opérateur.");;
-let secondValue = window.prompt('Tapez la valeur.');
+let firstValue = document.querySelector("first_number");
+let operator = document.querySelector("operator");
+let secondValue = document.querySelector("second_number");
+let calculate = document.querySelector("calculate");
+
+calculate.addEventListener(click, function(event){
+    console.log(firstValue);
+    console.log(secondValue);
+    console.log(operator);
+});
+
+
+
 
 function calculator(firstValue, operator, secondValue) {
 switch (operator) {
@@ -18,5 +28,7 @@ switch (operator) {
     console.log("Invalid operator");    
 }
 }
-calculator(firstValue, operator, secondValue);
+
+
+
 
